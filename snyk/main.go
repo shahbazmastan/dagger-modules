c1872b77824f8c376e87853e6e57baa2c024e70b
Shaik Shahbaz Mastan <shaikshahbaz.mastan@sony.com>
2026-02-04 14:09:06 +0530

    updated new

--- snyk/main.go
+++ snyk/main.go
@@ -1,17 +1,29 @@
 package main
 
-import "context"
+import (
+	"context"
 
-type Snyk struct{}
+	"github.com/shahbazmastan/dagger-modules/snyk/internal/dagger"
+)
 
-func New() *Snyk {
-	return &Snyk{}
+type Snyk struct {
+	Src *dagger.Directory
+}
+
+// New is the constructor.
+// Its parameters become flags on `dagger call` (NOT `dagger call new`).
+func New(
+	// Source directory to work with
+	src *dagger.Directory,
+) *Snyk {
+	return &Snyk{Src: src}
 }
 
 func (m *Snyk) Test(ctx context.Context) (string, error) {
-	return "i am running snyk test commands in snyk module\n", nil
+	// Just demonstrating that we have Src now
+	return "i am running snyk test commands in snyk module (src provided)\n", nil
 }
 
 func (m *Snyk) Monitor(ctx context.Context) (string, error) {
-	return "i am running snyk monitor commands in snyk module\n", nil
+	return "i am running snyk monitor commands in snyk module (src provided)\n", nil
 }
