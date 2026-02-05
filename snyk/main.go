07c54988da37372fa2aab9deaba3b7d83e295abd
Shaik Shahbaz Mastan <shaikshahbaz.mastan@sony.com>
2026-02-05 19:17:41 +0530

    added module and functions

--- snyk/main.go
+++ snyk/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"context"
-
 	"github.com/shahbazmastan/dagger-modules/snyk/internal/dagger"
 )
 
 type Snyk struct {
-	Src *dagger.Directory
+	Src      *dagger.Directory
+	Greeting string
+	Name     string
+	Age      int
 }
 
 // New is the constructor.
@@ -15,15 +16,38 @@ type Snyk struct {
 func New(
 	// Source directory to work with
 	src *dagger.Directory,
+	name string,
+	age int,
+	// Greeting message
+	// +optional
+	// +default="Hello"
+	greeting string,
 ) *Snyk {
-	return &Snyk{Src: src}
+	if greeting == "" {
+		greeting = "Hello" // defensive fallback
+	}
+
+	return &Snyk{
+		Src:      src,
+		Greeting: greeting,
+		Name:     name,
+		Age:      age,
+	}
 }
 
-func (m *Snyk) Test(ctx context.Context) (string, error) {
-	// Just demonstrating that we have Src now
-	return "i am running snyk test commands in snyk module (src provided)\n", nil
+func (m *Snyk) Monitor() string {
+	return m.Greeting + ", " + m.Name + "! You are " + string(rune(m.Age)) + " years old."
 }
 
-func (m *Snyk) Monitor(ctx context.Context) (string, error) {
-	return "i am running snyk monitor commands in snyk module (src provided)\n", nil
+func (m *Snyk) Test() string {
+	return m.Greeting + ", " + m.Name + "! You are " + string(rune(m.Age)) + " years old."
 }
+
+// func (m *Snyk) Test(ctx context.Context) (string, error) {
+// 	// Just demonstrating that we have Src now
+// 	return "i am running snyk test commands in snyk module (src provided)\n", nil
+// }
+
+// func (m *Snyk) Monitor(ctx context.Context) (string, error) {
+// 	return "i am running snyk monitor commands in snyk module (src provided)\n", nil
+// }
